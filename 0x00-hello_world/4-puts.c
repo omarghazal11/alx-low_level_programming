975ec8b086b048f3ac79614524fb38d326c7935f
omarghazal11 <omarghezo@gmail.com>
2023-08-10 20:55:32 -0700

    scripts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-int main(void)
+/**
+ * main - Entry point
+ * Description = A C programe that prints with put function 
+ * Return : Always 0 (success)
+*/
 {
-     puts ("\"Programming is like building a multilingual puzzle');
-     return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
